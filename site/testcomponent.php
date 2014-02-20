1e1d310268c437c2f05626d45504d78135ee3a38
jtester <joomlatester5@gmail.com>
2014-02-20 09:02:27 -0500

    Add com_testcomponent

---
+++ site/testcomponent.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package    TestComponent
+ * @subpackage Base
+ * @author     Nikolai Plath {@link https://github.com/elkuku}
+ * @author     Created on 20-Feb-2014
+ * @license    GNU/GPL
+ */
+
+//-- No direct access
+defined('_JEXEC') || die('=;)');
+
+echo 'Hello TEST';
